e50302f5898c25ccf083438f985ea07c713cb832
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-02-27 12:23:46 +0700

    commit

---
+++ PersegiPanjangDemo.java
@@ -0,0 +1,25 @@
+/**
+ * PersegiPanjangDemo
+ */
+public class PersegiPanjangDemo {
+
+    public static void main(String[] args) {
+        Praktikum03.PersegiPanjang[] arrayOfPersegiPanjang = new Praktikum03.PersegiPanjang[3];
+
+        arrayOfPersegiPanjang[0] = new Praktikum03.PersegiPanjang();
+        arrayOfPersegiPanjang[0].panjang = 110;
+        arrayOfPersegiPanjang[0].lebar = 30;
+    
+        arrayOfPersegiPanjang[1] = new Praktikum03.PersegiPanjang();
+        arrayOfPersegiPanjang[1].panjang = 80;
+        arrayOfPersegiPanjang[1].lebar = 40;
+    
+        arrayOfPersegiPanjang[2] = new Praktikum03.PersegiPanjang();
+        arrayOfPersegiPanjang[2].panjang = 100;
+        arrayOfPersegiPanjang[2].lebar = 20;
+    
+        System.out.println("Persegi panjang ke-1, panjang: " + arrayOfPersegiPanjang[0].panjang + ", lebar: " + arrayOfPersegiPanjang[0].lebar);
+        System.out.println("Persegi panjang ke-2, panjang: " + arrayOfPersegiPanjang[1].panjang + ", lebar: " + arrayOfPersegiPanjang[1].lebar);
+        System.out.println("Persegi panjang ke-3, panjang: " + arrayOfPersegiPanjang[2].panjang + ", lebar: " + arrayOfPersegiPanjang[2].lebar);
+    }
+}
---
+++ Praktikum03.java
@@ -0,0 +1,12 @@
+/**
+ * Praktikum03
+ */
+public class Praktikum03 {
+
+    public static class PersegiPanjang {
+    public int panjang;
+    public int lebar;
+}
+
+  
+}
